626500eb82bd73b50b1d6f8ec295c201eb349ef3
jurij <Jurij.Schaber@yahoo.de>
2025-09-13 11:47:44 +0200

    1st try

---
+++ maketable3.py
@@ -0,0 +1,56 @@
+from categoryfunctions import modellzuordnung, sonderausstattunggesamt, fuel, transmission, twoor4wheel, categorizekm, hasACC, haspano, dealer, isdamaged
+import pandas as pd
+import json
+
+import numpy as np
+from matplotlib import pyplot as plt
+bmw300251= open('m3.json',)
+bmw30025 = json.load(bmw300251)
+data1 = bmw30025
+
+bmw3er=open("bmw30025.json")
+bmw3er1=json.load(bmw3er)
+data=bmw3er1
+
+#auto = bmw30025[35]
+
+dfgesamt = pd.DataFrame(columns=['Modell','Sonderausstattung','Transmission','Wheeldrive', 'km','ACC','Pano','Dealer'])
+
+for auto in bmw30025:
+
+    if isdamaged(auto) == True:
+        continue
+
+    modell = modellzuordnung(auto) #12
+
+    if modell == "":
+        continue
+
+    sonderausstattung = sonderausstattunggesamt(auto) #5
+
+    transmission1 = transmission(auto) #2
+
+    if transmission =="":
+        continue
+    wheels = twoor4wheel(auto)   #2
+
+    kmcateogorie = categorizekm(auto) #4 (5)
+
+    ACC= hasACC(auto)              #2
+
+    pano = haspano(auto)           #2
+
+    dealer1 = dealer(auto)               #(2)
+
+    if dealer =="":
+        continue
+
+
+
+    dfgesamt.loc[len(dfgesamt)]=[modell, sonderausstattung, transmission1, wheels, kmcateogorie, ACC, pano, dealer1]
+
+print(dfgesamt)
+
+#for i in bmw3er1:
+#    print(isdamaged(i))
+
